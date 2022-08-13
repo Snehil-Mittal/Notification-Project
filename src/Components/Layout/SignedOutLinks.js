9d9bb1417c13196409e19c82a0fc43ca21ec2cf4
Snehil-Mittal <snehilmittal2001@gmail.com>
2022-08-13 15:52:27 +0530

    Formatting and Title Change

--- src/Components/Layout/SignedOutLinks.js
+++ src/Components/Layout/SignedOutLinks.js
@@ -1,12 +1,16 @@
-import React from 'react'
-import { NavLink } from 'react-router-dom'
+import React from 'react';
+import { NavLink } from 'react-router-dom';
 
-const SignedOutLinks = () =>{
-    return(
-        <ul className="right">
-            <li><NavLink to="/signin">Login</NavLink></li>
-            <li><NavLink to="/signup">Sign Up</NavLink></li>
-        </ul>
-    )
-}
-export default SignedOutLinks
+const SignedOutLinks = () => {
+	return (
+		<ul className="right">
+			<li>
+				<NavLink to="/signin">Login</NavLink>
+			</li>
+			<li>
+				<NavLink to="/signup">Sign Up</NavLink>
+			</li>
+		</ul>
+	);
+};
+export default SignedOutLinks;
--- src/index.js
+++ src/index.js
@@ -3,50 +3,60 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import './index.css';
-import {applyMiddleware, createStore,compose} from 'redux';
-import { Provider,useSelector } from 'react-redux';
+import { applyMiddleware, createStore, compose } from 'redux';
+import { Provider, useSelector } from 'react-redux';
 import rootreducer from './Store/Reducers/rootreducer';
-import thunk from 'redux-thunk'
-import {ReactReduxFirebaseProvider, getFirebase,isLoaded} from 'react-redux-firebase';
-import { createFirestoreInstance, getFirestore, reduxFirestore } from 'redux-firestore';
+import thunk from 'redux-thunk';
+import {
+	ReactReduxFirebaseProvider,
+	getFirebase,
+	isLoaded,
+} from 'react-redux-firebase';
+import {
+	createFirestoreInstance,
+	getFirestore,
+	reduxFirestore,
+} from 'redux-firestore';
 import fbConfig from './config/Fbconfig';
-import firebase from 'firebase/app'
-const store = createStore(rootreducer,
-  compose(
-  applyMiddleware(thunk.withExtraArgument({ getFirebase , getFirestore })),
-  reduxFirestore(firebase, fbConfig),
-  )
-  );
+import firebase from 'firebase/app';
+const store = createStore(
+	rootreducer,
+	compose(
+		applyMiddleware(thunk.withExtraArgument({ getFirebase, getFirestore })),
+		reduxFirestore(firebase, fbConfig)
+	)
+);
 
-  const rrfConfig = {
-    userProfile: 'users',
-    useFirestoreForProfile: true,
-    updateProfileOnLogin: false
-  }
-   const rrfProps = {
-      firebase,
-      config: rrfConfig,fbConfig,
-      dispatch: store.dispatch, 
-      createFirestoreInstance,
-     }
-     
-     function AuthIsLoaded({ children }) {
-      const auth = useSelector(state => state.firebase.auth)
-      if (!isLoaded(auth)) return <div>Loading Screen...</div>;
-          return children
-      }
-      
+const rrfConfig = {
+	userProfile: 'users',
+	useFirestoreForProfile: true,
+	updateProfileOnLogin: false,
+};
+const rrfProps = {
+	firebase,
+	config: rrfConfig,
+	fbConfig,
+	dispatch: store.dispatch,
+	createFirestoreInstance,
+};
+
+function AuthIsLoaded({ children }) {
+	const auth = useSelector((state) => state.firebase.auth);
+	if (!isLoaded(auth)) return <div>Loading Screen...</div>;
+	return children;
+}
+
 ReactDOM.render(
-  <React.StrictMode> 
-    <Provider store={store}>
-        <ReactReduxFirebaseProvider {...rrfProps}>
-        <AuthIsLoaded>
-          <App />
-        </AuthIsLoaded>
-        </ReactReduxFirebaseProvider> 
-    </Provider>
-  </React.StrictMode>,
-  document.getElementById('root')
+	<React.StrictMode>
+		<Provider store={store}>
+			<ReactReduxFirebaseProvider {...rrfProps}>
+				<AuthIsLoaded>
+					<App />
+				</AuthIsLoaded>
+			</ReactReduxFirebaseProvider>
+		</Provider>
+	</React.StrictMode>,
+	document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
