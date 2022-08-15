8a0723cbb854eb4572ac58ff521f208c1f63110c
Snehil-Mittal <snehilmittal2001@gmail.com>
2022-08-15 12:57:17 +0530

    Deployed Working Code

--- src/config/Fbconfig.js
+++ src/config/Fbconfig.js
@@ -3,9 +3,10 @@ import 'firebase/firestore';
 import 'firebase/auth';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-var firebaseConfig = {
-	apiKey: 'AIzaSyA3ZOxE9pWrPjIeO6wD-5RxnyGL5firU5a344',
+const firebaseConfig = {
+	apiKey: 'AIzaSyA3ZOxE9pWrPjIeO6wD-5RxnyGL5U5a344',
 	authDomain: 'notification-project-af221.firebaseapp.com',
+	databaseURL: 'https://notification-project-af221-default-rtdb.firebaseio.com',
 	projectId: 'notification-project-af221',
 	storageBucket: 'notification-project-af221.appspot.com',
 	messagingSenderId: '277444523867',
